2cbc84310412593bedf6be70c593a8025b432331
Russell Johnson <russell.johnson@livio.io>
2019-09-20 11:26:10 -0400

    added ts config

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+//TODO https://github.com/russjohnson09/sdl_javascript_suite/blob/develop/GUIDELINES.md#module-exports--imports
+//TODO: should this use module.exports or export {} syntax?
+//Which of these are node specific and which are common?
+//What are the capabilities of browsers to do the rpc creation?
+
+
+//TODO why is esm not included in the main package.json. Why is the package.json not in the root directory.
+//examples hello-sdl works but only because it is handling dependencies on its own in a way that wouldn't be
+//expected for someone using this package.
+
+require = require("esm")(module/*, options*/)
+
+module.exports = {
+  helloWorld: function()
+  {
+      console.log(`hello world`);
+  },
+    SdlPacket: require('./dist/js/protocol/SdlPacket'),
+
+};
