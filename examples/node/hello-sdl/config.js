5e82e4aa6d059d19c09e13c86a78bbe7d192ccbe
Christopher Rokita <chris@livio.io>
2019-12-12 10:27:28 -0500

    Simplify the example apps to use the LCM

--- examples/node/hello-sdl/config.js
+++ examples/node/hello-sdl/config.js
@@ -1,10 +1,5 @@
-const SDL = require('../../../lib/node/src/index.js');
-
 module.exports = {
     appId: 'hello-node',
     appName: 'hello-node',
     port: process.env.APP_PORT || 3005,
-    timeout: 5000,
-    ssl: new SDL.transport.SslConfig(),
-    appLifespan: process.env.APP_LIFESPAN || 5, // automatically closes the app after 5 seconds
 };
