79c1bab8d009ac98ea2c8b85936a75e1994a8505
Vladyslav Mustafin <vmustafin@luxoft.com>
2020-02-19 13:56:17 +0200

    Apply suggestions from code review

--- examples/webengine/hello-sdl/manifest.js
+++ examples/webengine/hello-sdl/manifest.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line camelcase,no-unused-vars
 const sdl_manifest = {
-    'entrypoint': 'index.html',
-    'appIcon': 'test_icon_1.png',
+    'entrypoint': './index.html',
+    'appIcon': './test_icon_1.png',
     'appID': 'hello-webengine',
     'appName': 'Hello WebEngine',
     'category': 'DEFAULT',
@@ -9,7 +9,7 @@ const sdl_manifest = {
     'locales': {
         'de_DE': {
             'appName': 'Hallo JS',
-            'appIcon': 'test_icon_1.png',
+            'appIcon': './test_icon_1.png',
         },
     },
     'appVersion': '1.0.0',
