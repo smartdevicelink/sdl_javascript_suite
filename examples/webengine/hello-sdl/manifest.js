8db136c86971e1ef332e5876ed7981aab7fc7f25
Nick Schwab <nick.schwab@livio.io>
2020-02-20 16:38:59 -0500

    + webengine example app fixes to accommodate Core/HMI team's implementation

--- examples/webengine/hello-sdl/manifest.js
+++ examples/webengine/hello-sdl/manifest.js
@@ -1,18 +1,18 @@
-// eslint-disable-next-line camelcase,no-unused-vars
-const sdl_manifest = {
-    'entrypoint': './index.html',
-    'appIcon': './test_icon_1.png',
-    'appID': 'hello-webengine',
-    'appName': 'Hello WebEngine',
-    'category': 'DEFAULT',
-    'additionalCategories': [],
-    'locales': {
-        'de_DE': {
-            'appName': 'Hallo JS',
-            'appIcon': './test_icon_1.png',
-        },
+/* eslint-disable */
+export default {
+    "entrypoint": "./index.html",
+    "appIcon": "./test_icon_1.png",
+    "appId": "hello-webengine2",
+    "appName": "Hello WebEngine2",
+    "category": "DEFAULT",
+    "additionalCategories": [],
+    "locales": {
+        "de_DE": {
+            "appName": "Hallo JS",
+            "appIcon": "./test_icon_1.png"
+        }
     },
-    'appVersion': '1.0.0',
-    'minRPCVersion': '6.0',
-    'minProtocolVersion': '5.0',
+    "appVersion": "1.0.0",
+    "minRpcVersion": "6.0",
+    "minProtocolVersion": "5.0"
 };
