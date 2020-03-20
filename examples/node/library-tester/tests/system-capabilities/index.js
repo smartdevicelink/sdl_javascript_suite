349a16f604f0292ff0561727d5bc469e5b92a433
Christopher Rokita <chris@livio.io>
2020-03-20 13:56:46 -0400

    Add system capability tests

---
+++ examples/node/library-tester/tests/system-capabilities/index.js
@@ -0,0 +1,81 @@
+/*
+* Copyright (c) 2019, Livio, Inc.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+*
+* Redistributions in binary form must reproduce the above copyright notice,
+* this list of conditions and the following
+* disclaimer in the documentation and/or other materials provided with the
+* distribution.
+*
+* Neither the name of the Livio Inc. nor the names of its contributors
+* may be used to endorse or promote products derived from this software
+* without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*/
+
+const SDL = require('../../SDL.min.js');
+const AppHelper = require('../../AppHelper.js');
+
+module.exports = async function (catalogRpc) {
+    const appId = 'system-capabilities';
+
+    const appConfig = new SDL.manager.AppConfig()
+        .setAppId(appId)
+        .setAppName(appId)
+        .setIsMediaApp(false)
+        .setLanguageDesired(SDL.rpc.enums.Language.EN_US)
+        .setHmiDisplayLanguageDesired(SDL.rpc.enums.Language.EN_US)
+        .setAppTypes([
+            SDL.rpc.enums.AppHMIType.MEDIA,
+            SDL.rpc.enums.AppHMIType.REMOTE_CONTROL,
+        ])
+        .setTransportConfig(new SDL.transport.TcpClientConfig(process.env.HOST, process.env.PORT));
+
+    const app = new AppHelper(catalogRpc)
+        .setAppConfig(appConfig);
+
+    await app.start(); // after this point, we are in HMI FULL and managers are ready
+    const sdlManager = app.getManager();
+    // check for the RAIR
+    if (!(sdlManager.getRegisterAppInterfaceResponse() instanceof SDL.rpc.messages.RegisterAppInterfaceResponse)) {
+        console.log('RegisterAppInterfaceResponse not present in the lifecycle manager!');
+    }
+
+    // retrieve the capabilities
+    const scm = sdlManager.getSystemCapabilityManager();
+    for (const sct in SDL.rpc.enums.SystemCapabilityType._MAP) {
+        await scm.queryCapability(sct);
+    }
+
+    // read the display capabilities object returned
+    // const displayCapabilities = await scm.queryCapability(SDL.rpc.enums.SystemCapabilityType.DISPLAYS);
+
+    // tear down the app
+    await sdlManager.sendRpc(new SDL.rpc.messages.UnregisterAppInterface());
+    sdlManager.dispose();
+};
+
+
+
+function sleep (timeout = 1000) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, timeout);
+    });
+}
