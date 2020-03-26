c89219c20236cf5711bb0ee448f45492ad25ca0d
Christopher Rokita <chris@livio.io>
2020-03-26 16:47:49 -0400

    Update weather app service test to check notification updates

--- examples/node/library-tester/tests/app-services-weather/index.js
+++ examples/node/library-tester/tests/app-services-weather/index.js
@@ -45,17 +45,23 @@ module.exports = async function (catalogRpc) {
     const consumer = new Consumer(catalogRpc);
     await consumer.start();
 
+    // get the initial number of app services connected before continuing
+    const prePublishAppServices = await consumer.sdlManager.getSystemCapabilityManager().queryCapability(SDL.rpc.enums.SystemCapabilityType.APP_SERVICES);
+    const BASE_SERVICES_CONNECTED = prePublishAppServices.length;
+
     // publish the app services and have the consumer listen for when it happens
     await producer1.setupAppService(true);
+    await expectAppServiceCount(consumer.sdlManager, BASE_SERVICES_CONNECTED + 1);
     await producer2.setupAppService(true);
+    await expectAppServiceCount(consumer.sdlManager, BASE_SERVICES_CONNECTED + 2);
 
-    let appServiceCapabilities = await consumer.sdlManager.getSystemCapabilityManager().queryCapability(SDL.rpc.enums.SystemCapabilityType.APP_SERVICES);
+    const appServiceCapabilities = await consumer.sdlManager.getSystemCapabilityManager().queryCapability(SDL.rpc.enums.SystemCapabilityType.APP_SERVICES);
 
     // locate the weather app services from the capability response and use the service ids to activate one of them
     const foundCapability = appServiceCapabilities.find((capability) => {
         return 'weather-service-1' === capability.getUpdatedAppServiceRecord()
             .getServiceManifest()
-            .getServiceName()
+            .getServiceName();
     });
     const weatherId = foundCapability.getUpdatedAppServiceRecord().getServiceID();
     await consumer.sendPerformInteractionPromise(weatherId);
@@ -71,13 +77,15 @@ module.exports = async function (catalogRpc) {
     const weatherImage = appDataResponse.getServiceData().getWeatherServiceData().getCurrentForecast().getWeatherIcon();
 
     await consumer.getAndShowImage(weatherImage, weatherId);
- 
+
     // now update the producer1 app service to not allow consumers to read it
     await producer1.setupAppService(false);
 
     // now unpublish both services
     await producer1.unpublishAppService();
+    await expectAppServiceCount(consumer.sdlManager, BASE_SERVICES_CONNECTED + 1);
     await producer2.unpublishAppService();
+    await expectAppServiceCount(consumer.sdlManager, BASE_SERVICES_CONNECTED);
 
     await sleep(4000);
 
@@ -85,6 +93,24 @@ module.exports = async function (catalogRpc) {
     await producer2.stop();
     await consumer.stop();
 };
+
+function expectAppServiceCount (sdlManager, number) {
+    return new Promise((resolve, reject) => {
+        const listener = (appServiceCapabilities) => {
+            if (appServiceCapabilities.getAppServices().length === number) {
+                resolve();
+            } else {
+                reject(`Expected app service count to be ${number}: got ${appServiceCapabilities.getAppServices().length}`);
+            }
+            // stop the listener
+            sdlManager.getSystemCapabilityManager()
+                .removeOnSystemCapabilityListener(SDL.rpc.enums.SystemCapabilityType.APP_SERVICES, listener);
+        };
+
+        sdlManager.getSystemCapabilityManager()
+            .addOnSystemCapabilityListener(SDL.rpc.enums.SystemCapabilityType.APP_SERVICES, listener);
+    });
+}
 
 function sleep (timeout = 1000) {
     return new Promise((resolve) => {
