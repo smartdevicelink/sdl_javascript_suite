ed886fecc5575fadfcd1aa82c2064eab1c9de95d
Christopher Rokita <chris@livio.io>
2020-04-23 14:06:00 -0400

    Remove RpcType in favor of MessageType

--- generator/templates/RpcCreator_template.js
+++ generator/templates/RpcCreator_template.js
@@ -4,7 +4,7 @@
 // messages
 {{-super()}}
 // other
-import { RpcType } from './enums/RpcType.js';
+import { MessageType } from './enums/MessageType.js';
 import { FunctionID } from './enums/FunctionID.js';
 import { JsonRpcMarshaller } from './../util/JsonRpcMarshaller.js';
 import { _BinaryFrameHeader } from './../protocol/_BinaryFrameHeader.js';
@@ -20,8 +20,8 @@ import { _BinaryFrameHeader } from './../protocol/_BinaryFrameHeader.js';
         const binaryFrameHeader = _BinaryFrameHeader.fromBinaryHeader(payload);
 
         let message;
-        const rpcType = binaryFrameHeader.getRpcType();
-        const rpcName = RpcType.keyForValue(rpcType);
+        const messageType = binaryFrameHeader.getMessageType();
+        const rpcName = MessageType.keyForValue(messageType);
         const correlationId = binaryFrameHeader.getCorrelationId();
         const functionId = binaryFrameHeader.getFunctionId();
         const functionName = FunctionID.keyForValue(functionId);
@@ -36,9 +36,9 @@ import { _BinaryFrameHeader } from './../protocol/_BinaryFrameHeader.js';
         switch (functionId) {
             {%- for item in cases %}
             case FunctionID.{{item.function_name}}:
-                if (rpcType === RpcType.{{item.type}}) {
+                if (messageType === MessageType.{{item.type|lower}}) {
                     message = new {{item.class_name}}(params);
-                }{% if item.type == 'REQUEST' %} else if (rpcType === RpcType.RESPONSE) {
+                }{% if item.type == 'REQUEST' %} else if (messageType === MessageType.response) {
                     message = new {{item.class_name}}Response(params);
                 }
                 {%- endif %}
@@ -53,7 +53,7 @@ import { _BinaryFrameHeader } from './../protocol/_BinaryFrameHeader.js';
             return null;
         }
 
-        if (rpcType === RpcType.REQUEST || rpcType === RpcType.RESPONSE) {
+        if (messageType === MessageType.request || messageType === MessageType.response) {
             message.setCorrelationId(correlationId);
         }
         if (bulkData) {
