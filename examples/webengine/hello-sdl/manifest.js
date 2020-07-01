d67034910f2a534728e10c727112bafbc4cbb4f6
Vladyslav Mustafin <vmustafin@luxoft.com>
2020-07-01 17:54:00 +0300

    Update webengine/hello-sdl application to be ready for the new App HMI type WEB_VIEW

--- examples/webengine/hello-sdl/manifest.js
+++ examples/webengine/hello-sdl/manifest.js
@@ -4,7 +4,7 @@ export default {
     "appIcon": "./test_icon_1.png",
     "appId": "hello-webengine2",
     "appName": "Hello WebEngine2",
-    "category": "DEFAULT",
+    "category": "WEB_VIEW",
     "additionalCategories": [],
     "locales": {
         "de_DE": {
@@ -13,6 +13,6 @@ export default {
         }
     },
     "appVersion": "1.0.0",
-    "minRpcVersion": "6.0",
-    "minProtocolVersion": "5.0"
+    "minRpcVersion": "6.2.0",
+    "minProtocolVersion": "5.0.0"
 };
