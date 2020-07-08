8a6f5655a2d18a65bae2c91b9977cfc276dd76e3
Vladyslav Mustafin <vmustafin@luxoft.com>
2020-07-08 21:24:56 +0300

    Adjusted GetVehicleDataResponseTests.js to use Test.js constants

--- tests/node/rpc/messages/GetVehicleDataResponseTests.js
+++ tests/node/rpc/messages/GetVehicleDataResponseTests.js
@@ -2,46 +2,21 @@ const SDL = require('./../../../../lib/js/dist/SDL.min.js');
 const GetVehicleDataResponse = SDL.rpc.messages.GetVehicleDataResponse;
 const FunctionID = SDL.rpc.enums.FunctionID;
 const MessageType = SDL.rpc.enums.MessageType;
-const WindowStatus = SDL.rpc.structs.WindowStatus;
-const WindowState = SDL.rpc.structs.WindowState;
-const Grid = SDL.rpc.structs.Grid;
 
 const BaseRpcTests = require('./BaseRpcTests');
 const Test = require('./../../../Test.js');
 const Validator = require('./../../../Validator.js');
 
-
 describe('GetVehicleDataResponseTests', function () {
     before(function () {
-        const windowState = new WindowState()
-            .setApproximatePosition(Test.GENERAL_INTEGER)
-            .setDeviation(Test.GENERAL_INTEGER);
-
-        const JSON_WINDOW_STATE = windowState.getParameters();
-
-        const grid = new Grid()
-            .setColumn(Test.GENERAL_INTEGER)
-            .setRow(Test.GENERAL_INTEGER);
-
-        const JSON_GRID = grid.getParameters();
-
-        this.windowStatus = new WindowStatus()
-            .setLocation(grid)
-            .setState(windowState);
-
-        const JSON_WINDOWSTATUS = {
-            [WindowStatus.KEY_LOCATION]: JSON_GRID,
-            [WindowStatus.KEY_STATE]: JSON_WINDOW_STATE,
-        };
-
         this.createMessage = function () {
             return new GetVehicleDataResponse()
-                .setWindowStatus([this.windowStatus]);
+                .setWindowStatus([Test.GENERAL_WINDOW_STATUS]);
         };
 
         this.getExpectedParameters = function (sdlVersion) {
             return {
-                [GetVehicleDataResponse.KEY_WINDOW_STATUS]: [JSON_WINDOWSTATUS],
+                [GetVehicleDataResponse.KEY_WINDOW_STATUS]: [Test.JSON_WINDOWSTATUS],
             };
         };
 
@@ -62,7 +37,7 @@ describe('GetVehicleDataResponseTests', function () {
         const testWindowStatus = rpcMessage.getWindowStatus();
 
         // Valid Tests
-        Validator.assertEquals([this.windowStatus], testWindowStatus);
+        Validator.assertEquals([Test.GENERAL_WINDOW_STATUS], testWindowStatus);
 
         // Invalid/Null Tests
         rpcMessage = new GetVehicleDataResponse();
