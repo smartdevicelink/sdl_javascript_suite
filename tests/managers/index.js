24a00d4a293e904b571fdedc90364d238f7f539c
renonick87 <renonick87@gmail.com>
2020-10-14 09:48:18 -0400

    Update unit tests to cover TextAndGraphicManager and TextAndGraphicUpdateOperation

--- tests/managers/index.js
+++ tests/managers/index.js
@@ -7,6 +7,8 @@ const lifecycleManagerTests = require('./lifecycle/LifecycleManagerTests');
 const fileManagerTests = require('./file/FileManagerTests');
 const taskTests = require('./TaskTests');
 const queueTests = require('./QueueTests');
+const textAndGraphicManagerTests = require('./screen/TextAndGraphicManagerTests');
+const textAndGraphicUpdateOperationTests = require('./screen/TextAndGraphicUpdateOperationTests');
 
 // connect to core and select the app on the HMI to run the tests
 describe('ManagerTests', function () {
@@ -16,13 +18,15 @@ describe('ManagerTests', function () {
         appWebSocketServer.on('connection', function (connection) {
             console.log('Connection');
             const appClient = new AppClient(connection, async (teardown) => {
+                await textAndGraphicManagerTests(appClient);
                 permissionManagerTests(appClient);
                 softButtonManagerTests(appClient);
-                screenManagerTests(appClient);
+                await screenManagerTests(appClient);
                 lifecycleManagerTests(appClient);
                 fileManagerTests(appClient);
                 taskTests(appClient);
                 queueTests(appClient);
+                textAndGraphicUpdateOperationTests(appClient);
                 setTimeout(function () {
                     teardown();
                     done();
